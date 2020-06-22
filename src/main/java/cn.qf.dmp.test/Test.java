a4bb081e146c3134c3b82abee8e08cf90645b168
Mei12 <1738505856@qq.com>
2020-06-22 17:40:53 +0800

    test2

--- src/main/java/cn.qf.dmp.test/Test.java
+++ src/main/java/cn.qf.dmp.test/Test.java
@@ -3,5 +3,9 @@ package cn.qf.dmp.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+
     }
 }
